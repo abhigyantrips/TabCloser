46466271b7b4c8dc568fae06423e852d24656d6c
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-04-25 15:06:24 +0530

    feat(config): add extension config

---
+++ extension.config.ts
@@ -0,0 +1,6 @@
+export type ExtensionConfig = typeof extensionConfig;
+
+export const extensionConfig = {
+  name: 'Tab Closer',
+  description: 'Quite literally, closes a tab as soon as you try to visit a URL you blocked.',
+};
